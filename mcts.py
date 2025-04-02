2c2f6eb50a23d0e6cdf35c0a662c1d3bda2d77a8
Nishil Patel <nishil.patel13@gmail.com>
2025-04-02 15:10:08 -0400

    mcts prototype?

--- mcts.py
+++ mcts.py
@@ -1,23 +1,75 @@
 import random
 import time
+import math
 import collections
 from uttt import UTTT
 
-REWARDS = { 
-    "X": 1,
-    "O": -1,
-    "T": 0,
-    None: 0,
-}
+EXPLORATION = 1.5
+ITERATIONS = 100
 
-def rollout(moves): return random.choice(moves)
+def reward(winner, player):
+    if winner == "T": return 0
+    if winner == player: return 1
+    return -1
 
-def simulate(state: UTTT):
-    """
-    Returns the winner of the given game played to completion.
-    Moves are made based on the rollout function
-    """
+#------------------------ Tree nodes --------------------------#
+
+class Node:
+    def __init__(self, state: UTTT) -> None:
+        self.state = state
+        self.visits = 0
+        self.val = 0
+        self.children = []
+
+    def expand_children(self):
+        moves = self.state.get_valid_moves()
+        self.children = [Node(self.state.make_move(*move)) for move in moves]
+
+    def update_result(self, reward):
+        self.visits += 1
+        self.val += reward
+
+def ucb(node: Node, parent_visits):
+    if parent_visits == 0 or node.visits == 0: return float('inf')
+    exploitation = node.val / node.visits
+    exploration = EXPLORATION * math.sqrt(math.log(parent_visits) / node.visits)
+    return exploitation + exploration
+
+#------------------------ Phases --------------------------#
+
+def select(node: Node) -> list[Node]:
+    path = [node]
+    while node.children:
+        node = max(node.children, key=lambda n: ucb(n, node.visits))
+        path.append(node)
+    return path
+
+def expand(node: Node):
+    if node.state.game_winner(): return None
+    node.expand_children()
+    return random.choice(node.children)
+
+def simulate(node: Node, player: str, rollout = random.choice):
+    state = node.state
     while not state.game_winner():
         move = rollout(state.get_valid_moves())
         state = state.make_move(*move)
-    return state.game_winner()
+    return reward(state.game_winner(), player)
+
+def back_propogation(path: list[Node], reward):
+    for n in path: n.update_result(reward)
+
+#------------------------ Move Prediction --------------------------#
+
+def update_tree(root: Node, player: str):
+    path = select(root)
+    expanded = expand(path[-1])
+    if expanded: path.append(expanded)
+    reward = simulate(path[-1], player)
+    back_propogation(path, reward)
+
+def predict(state: UTTT, player: str):
+    root = Node(state)
+    for _ in range(ITERATIONS): update_tree(root, player)
+    best_node = max(root.children, key=lambda node: node.val)
+    return best_node.state
